aabeaa2133d6fd9edb3e312b51ea567f2a3e9c58
Christine Gierer <beansprout@users.noreply.github.com>
2017-04-15 13:53:19 -0400

    bubblesort works

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -19,8 +19,40 @@
  * Example:
  * bubbleSort([2, 1, 3]); // returns [1, 2, 3]
  *
+ procedure bubbleSort( A : list of sortable items ) defined as:
+  do
+    swapped := false
+    for each i in 0 to length(A) - 2 inclusive do:
+      if A[i] > A[i+1] then
+        swap( A[i], A[i+1] )
+        swapped := true
+      end if
+    end for
+  while swapped
+end procedure
 */
 
-const bubbleSort = (arr) => {
 
+const bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped === true);
+  return arr;
 };
+
+
+// test cases
+// arr= [2, 1, 3, 8];
+//= > [1, 2, 3, 8]
+// let a = [8, 23, 1, 100, 23, 0];
+// // = > [0, 1, 8, 23, 23, 100]
+// console.log(bubbleSort(a));
