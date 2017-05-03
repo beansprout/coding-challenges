e1f46d40e30167bf04b40b796bed322dace1fd43
Christine Gierer <beansprout@users.noreply.github.com>
2017-05-03 12:10:24 -0400

    05.03.17

--- largetsContiguousSum/largestContiguousSum.js
+++ largetsContiguousSum/largestContiguousSum.js
@@ -4,4 +4,12 @@
  * example 1: sumArray([1, 2, 3]); // => 6
  * example 2: sumArray([4, -1, 5]); // => 8
  * example 3: sumArray([10, -11, 11]); // 11
- */
+ *
+1) Divide the given array in two halves
+2) Return the maximum of following three
+….a) Maximum subarray sum in left half (Make a recursive call)
+….b) Maximum subarray sum in right half (Make a recursive call)
+….c) Maximum subarray sum such that the subarray crosses the midpoint
+
+ *
+*/
