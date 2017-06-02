ed659732efff375676df06ab5e059202ef4341e6
Christine Gierer <beansprout@users.noreply.github.com>
2017-06-02 12:02:42 -0400

    updated 06.02.17

--- validateBST/validateBST.js
+++ validateBST/validateBST.js
@@ -5,3 +5,62 @@
  * values in sorted order.
  *
  */
+// A binary tree:
+{
+  value: 15,
+  left: {
+          value: 7,
+          left: {
+                  value: 1,
+                  left: null,
+                  right: null
+                 },
+          right: {
+                   value: 12,
+                   left: null,
+                   right: null
+                 }
+        },
+  right: null
+}
+
+//a binary tree as an array of tuples
+var tree = [15, [7, [1, null, null], [12, null, null]], null]
+//each tuple is structured as [value, left, right]:
+
+// check each branch of the tree
+
+/* treeObj = {
+             left: {
+                lnode1: val,
+                lnode2: val
+                lnode3: {
+                  ln3left: {
+                    node4: val,
+                    node5: val
+                  },
+                  ln3right: {
+                    n6: val
+                  }
+                },
+                right: {
+                  rnode1: {
+                    rn1left: {
+                      rn4: val,
+                      rn5: val,
+                      rn6: val
+                    },
+                    rn1right: {
+                      rn7: val
+                    }
+                  },
+                  rnode2: val
+                }
+              }
+
+
+
+const validateBST = (tree) => {
+
+}
+
