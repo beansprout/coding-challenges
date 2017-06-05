a91d43a306dd3905e989c8e65a0b9a1cbcf0ab3c
Christine Gierer <beansprout@users.noreply.github.com>
2017-06-05 12:31:49 -0400

    updated ..dd

--- routeBetweenNodes/routeBetweenNodes.js
+++ routeBetweenNodes/routeBetweenNodes.js
@@ -2,3 +2,21 @@
  * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
  * Google "directed graph" and look under images for visual examples.
  */
+
+// graph structure:
+
+
+/* func (nodeA, nodeB) {
+  if an in from nodeA matches an out from nodeB then true
+  if an in from nodeB matches an out from NodeA then true
+*/
+
+class directedGraph {
+  constructor (node) {
+
+  }
+}
+
+const routeBetweenNodes = (nodeA, nodeB) => {
+  if (nodeA.ins.hasOwnProperty(nodeB.outs)
+  }
