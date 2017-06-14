3fe48dc93cabe966926813b31191d305e96170f4
beansprout <christinegierer@gmail.com>
2017-06-14 13:11:19 -0400

    update

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -1,3 +1,11 @@
 /* Implement an algorithm to determine if a string has all unique characters. 
  * What if you cannot use additional data structures?
  */
+
+// make helper func regex to check for unique characters
+const x = " a v x f h d sde";
+if (x === (^(GO(?: ([A-Z])(?!.*\2))+)$)) return true;
+
+
+// func check for unique
+//  
