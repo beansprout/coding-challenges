4c31a4d47b24e345c5949a29de90a5b592e2d3e6
Christine Gierer <christinegierer@gmail.com>
2017-06-16 13:58:20 -0400

    constructor challenge updated

--- constructors/constructors.js
+++ constructors/constructors.js
@@ -21,3 +21,58 @@
  * This is how you would structure the game objects in an actual game 
  * application in Unity or another similar framework.
  */
+
+// NPC should be a general class for a non-player character in the game.
+// Level 1 (Top Level)
+ class NPC {
+     constructor(
+         hp, 
+         strength, 
+         speed, 
+         intellect, 
+         stamina, 
+         jerkishness, 
+         legNumber, 
+         diurnalOrNocturnal, 
+         sleepNeededNumHrs
+         )
+            {
+                this.hp = hp;
+                this.strength = strength;
+                this.speed = speed;
+                this.intellect = intellect;
+                this.stamina = stamina;
+                this.jerkishness = jerkishness;
+     }
+ };
+
+// Level 2 
+ // NPC -> Humanoid, Animal, Plant
+
+// Humanoids
+ class Humanoid extends NPC {
+     constructor( )
+
+ }
+
+//  * Humanoid -> Human, Elf, Orc
+
+ class Human extends Humanoid {
+
+ }
+
+ // Humans
+//  * Human -> Soldier, Peasant, Bandit
+// Animals
+//  * Animal -> Bear, Wolf
+// Plants
+//  * Plant -> FleshEatingDaisy
+
+// 
+ 
+//  Humanoid, Animal, and Plant should all inherit from NPC.  The classes
+//  shown to the right of the arrow are classes that will inherit
+//  from those classes.
+
+// NPC -> Humanoid, Animal, Plant
+//  *
